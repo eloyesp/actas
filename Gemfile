88651f38897a24578f0152e632f939fdc68ea55a
Eloy Espinaco <eloyesp@gmail.com>
2016-08-28 21:35:27 -0300

    Vistas

--- Gemfile
+++ Gemfile
@@ -2,5 +2,10 @@
 source 'https://rubygems.org'
 
 gem 'cuba'
-gem 'cutest'
-gem 'rack-test'
+gem 'slim'
+gem 'thin'
+
+group :development do
+  gem 'cutest'
+  gem 'rack-test'
+end
--- actas.rb
+++ actas.rb
@@ -1,15 +1,19 @@
 require 'cuba'
 require 'cuba/safe'
+require 'cuba/render'
+require 'slim'
 
 Cuba.use Rack::Session::Cookie, :secret => '__a_very_long_string__'
 
 Cuba.plugin Cuba::Safe
+Cuba.plugin Cuba::Render
+Cuba.settings[:render][:template_engine] = 'slim'
 
 Cuba.define do
   on get do
     on 'actas', 'nueva' do
       on root do
-        res.write 'Hello world!'
+        render('nueva_acta')
       end
     end
   end
